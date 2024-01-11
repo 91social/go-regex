cdf850aebdc83862f65892f0ef6cdef32c4c651d
Pavan Kumar <pavan@episilia.com>
2024-01-11 13:09:48 +0530

    cmake build command change

--- src/regextoken.go
+++ src/regextoken.go
@@ -2,28 +2,28 @@ package main
 
 import "C"
 import (
-    "log"
-    "encoding/json"
-    "github.com/google/codesearch/index"
-    "github.com/google/codesearch/regexp"
+	"encoding/json"
+	"github.com/google/codesearch/index"
+	"github.com/google/codesearch/regexp"
+	"log"
 )
 
-    //export tokens
-    func tokens(pat string) *C.char {
-        re, err := regexp.Compile(pat)
-        if err != nil {
-            log.Printf("Error Parsing regexp", err)
-            return C.CString(string(""))
-        } else {
-            q := index.RegexpQuery(re.Syntax)
+//export tokens
+func tokens(pat string) *C.char {
+	re, err := regexp.Compile(pat)
+	if err != nil {
+		log.Printf("Error Parsing regexp", err)
+		return C.CString(string(""))
+	} else {
+		q := index.RegexpQuery(re.Syntax)
 
-            log.Printf("The trigram query :: ",  pat,  q)
-            j, err := json.Marshal(q)
-            _ = err
-            return C.CString(string(j))
-        }
-    }
+		log.Printf("The trigram query :: ", pat, q)
+		j, err := json.Marshal(q)
+		_ = err
+		return C.CString(string(j))
+	}
+}
 
-    func main() {
-        tokens("^(?m) progra m|will$")
-    }
+func main() {
+	tokens("^(?m) progra m|will$")
+}
